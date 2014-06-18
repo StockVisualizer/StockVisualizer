128df5563f62bef9ade641a890761740ee8174fd
stevenmiller888 <stevenmiller888@me.com>
2014-06-18 01:06:11 -0700

    added search form and /search route grabs the input in the dashing.js file, need to send that data to a job that will update all of the widgets with the new stock

--- jobs/market_cap.job.js
+++ jobs/market_cap.job.js
@@ -1,3 +1,5 @@
+// need to update to only check during market hours
+
 setInterval(function() {
   var http = require("http");
   url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22AAPL%22%29%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
