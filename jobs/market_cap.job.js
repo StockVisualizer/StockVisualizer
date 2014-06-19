5290c333b04aea9694ca531bb1d05dce758475d2
stevenmiller888 <stevenmiller888@me.com>
2014-06-18 20:57:15 -0700

    Got search working, but have to fix some issues

--- jobs/market_cap.job.js
+++ jobs/market_cap.job.js
@@ -5,7 +5,7 @@
 // Run this once
 function callYahoo() {
     var http = require("http");
-    url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22AAPL%22%29%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
+    url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22" + process.env.ticker + "%22%29%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json"
     var request = http.get(url, function(response) {
         var buffer = "";
         var data;
@@ -14,7 +14,6 @@ function callYahoo() {
         });
         response.on("end", function(err) {
             data = JSON.parse(buffer);
-            // console.log("%j", data);
             current_valuation = data["query"]["results"]["quote"]["MarketCapitalization"];
             send_event('valuation', {
                 current: current_valuation
@@ -28,7 +27,7 @@ callYahoo();
 setInterval(function() {
     current_time = new Date().toString().substr(16, 8);
     current_time_formatted = parseInt(current_time.substr(0, 2) + current_time.substr(3, 2) + current_time.substr(6, 5));
-    if (current_time_formatted > 63000 && current_time_formatted < 130000) {
+    // if (current_time_formatted > 63000 && current_time_formatted < 130000) {
         callYahoo();
-    }
+    // }
 }, 2 * 1000);
